67f23e629d3bf8a048d28fe4e4ddbd0d0e0db69e
Vitor Natario <natariovitor@gmail.com>
2022-10-13 19:31:50 -0300

    config tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //configurando onde estam os arquivos de conteudo da aplicacao
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
